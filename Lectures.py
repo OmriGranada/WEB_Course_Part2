7e55347683e16d1f476b122f2509d4b7124ac324
omriGranada <omri.granada@gmail.com>
2021-12-22 21:33:35 +0200

    ex9

--- Lectures.py
+++ Lectures.py
@@ -1,12 +1,16 @@
 from flask import Flask, redirect, url_for
 from flask import render_template
+from flask import request
+from flask import session
+from interact_with_DB import interact_db
 
 app = Flask(__name__)
+app.secret_key = '123'
 
 @app.route('/home_page')
 @app.route('/home')
 @app.route('/')
-def home_page():
+def home_func():
     # DB
     found = False
     if found:
@@ -29,14 +33,64 @@ def about_func():
                            hobbies=('art', 'music', 'flask')) # tuple
 
 
-
 @app.route('/catalog')
 def catalog_func():
+    if 'user_inside' in session:
+        if session['user_inside']:
+            print('user inside')
+    if 'product' in request.args: # check if the form is filled
+        product = request.args['product'] # gets the value of the "name" from html
+        size = request.args['size']
+        return render_template('catalog.html', p_name = product, p_size = size)
     return render_template('catalog.html')
 
-@app.route('/login')
+@app.route('/logout')
+def logout_func():
+    session['username'] = ''
+    return render_template('index.html')
+
+@app.route('/delete_user', methods=['POST'])
+def delete_user_func():
+    user_id = request.form['id']
+    query = "DELETE FROM users WHERE id='%s';" % user_id
+    interact_db(query=query, query_type='commit')
+    return redirect('/users')
+
+@app.route('/insert_user', methods=['POST'])
+def insert_user_func():
+    # get the data
+    name = request.form['name']
+    email = request.form['email']
+    password = request.form['password']
+    # insert to db
+    query = "INSERT INTO users(name, email, password) VALUES ('%s', '%s', '%s');" % (name, email, password)
+    interact_db(query = query, query_type='commit')
+    # come back to users page
+    return redirect('/users')
+
+@app.route('/users')
+def users_func():
+    query = 'select * from users;'
+    users = interact_db(query = query, query_type='fetch' )
+    return render_template('users.html', users = users)
+
+@app.route('/login', methods=['GET', 'POST']) # need both get (for the first time) & post
 def login_func():
-    return redirect(url_for('home_func')) # call for another func - home
+    if request.method == 'GET':
+        return render_template('login.html')
+    if request.method == 'POST':
+        username = request.form['username'] # POST -> form, GET -> args
+        password = request.form['password']
+        # DB
+        found = True
+        if found:
+            session['username'] = username
+            session['user_inside'] = True
+            return redirect(url_for('home_func'))
+        else:
+            render_template('login.html')
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
@@ -46,9 +100,11 @@ if __name__ == '__main__':
 COMMENTS:
 * i left
 * GET (Select): it doesnt matter if we share the parameters we send
-* POST (Insert): we dont want to share the parameters er send
+* POST (Insert): we dont want to share the parameters we send
 * PUT (Update):
 * DELETE:
 * we use form in order to use those requests
+
+* blue prints
 
 """
--- app.py
+++ app.py
@@ -1,13 +1,22 @@
 from flask import Flask, redirect, url_for
 from flask import render_template
+from flask import request
+from flask import session
 
 app = Flask(__name__)
+app.secret_key ='123' # must be when we use session, for every project
+
+# Create list of users as a dictionary
+users = {'user1': {'name': 'Drori', 'email': 'drori@gmail.com'},
+         'user2': {'name': 'Orit', 'email': 'orit@gmail.com'},
+         'user3': {'name': 'Yotam', 'email': 'yotam@gmail.com'},
+         'user4': {'name': 'Omri', 'email': 'omri@gmail.com'},
+         'user5': {'name': 'Amitay', 'email': 'amitay@gmail.com'}}
 
 @app.route('/homePage')
 @app.route('/')
 def home_func():
     return render_template('index8.html')
-
 
 @app.route('/Assignment8')
 def Assignment8_func():
@@ -28,6 +37,42 @@ def Assignment8_func():
                            university=uni, # string
                            hobbies=('Running', 'Flask', 'Scuba-Diving')) # tuple
 
+@app.route('/LogOut')
+def LogOut_func():
+    session['username'] = ''
+    return render_template('Assignment9.html')
+
+def find_user(given_email):
+    print(given_email)
+    for user_number, user_details in users.items():
+        if user_details['email'] == given_email:
+            print('user found!!!!!!!!!!!!!!')
+            return user_details['name'], user_details['email']
+    print('didnt find user')
+    return False
+
+
+@app.route('/Assignment9', methods=['GET', 'POST'])
+def Assignment9_func():
+    if request.method == 'GET':
+        # SEARCH form
+        if 'user_email' in request.args:
+            if (find_user(request.args['user_email']) != False):
+                result_name = find_user(request.args['user_email'])[0]
+                result_email = find_user(request.args['user_email'])[1]
+                return render_template('Assignment9.html', result_name=result_name, result_email=result_email)
+            return render_template('Assignment9.html', users_List=users)# in case the email is incorrect or empty all of the esers will be displayed
+    if request.method == 'POST':
+        # REGISTER form
+        username = request.form['username']
+        # password = request.form['password']
+        # DB
+        found = True
+        if found: # fictive check
+            session['username'] = username
+            # session['user_inside'] = True
+            return render_template('Assignment9.html')
+    return render_template('Assignment9.html')
 
 
 if __name__ == '__main__':
